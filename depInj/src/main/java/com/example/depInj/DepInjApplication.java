004bbab0c2237fa6ea9d3dfded6e901e702d2ab6
Mistry <Tarun.Mistry@capco.com>
2018-03-21 10:31:14 -0400

    Initial commit

---
+++ depInj/src/main/java/com/example/depInj/DepInjApplication.java
@@ -0,0 +1,12 @@
+package com.example.depInj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepInjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepInjApplication.class, args);
+	}
+}
